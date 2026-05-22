a23e5e850775eb96d69ffae55070907d4a1cf4cd
alecamsan1 <alecamsan@alum.us.es>
2026-05-22 15:38:23 +0200

    Fix: corregido el import de nedb para el despliegue

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { Datastore } from 'nedb-promises'; // Versión moderna para módulos ES
+import Datastore from 'nedb-promises';
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,10 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 // Base de datos persistente en la raíz
-const db = Datastore.create({ filename: './marsi-agroapp.db', autoload: true });
+//  ASÍ ES LO CORRECTO
 
 const BASE_API_URL = "/api/v1/marsi-agroapp";
-
+const db = Datastore.create({ filename: './marsi-agroapp.db', autoload: true });
 // Endpoint de prueba / Datos iniciales
 app.get(`${BASE_API_URL}/loadInitialData`, async (req, res) => {
     const docs = await db.find({});
@@ -31,7 +31,19 @@ app.get(BASE_API_URL, async (req, res) => {
     const data = await db.find({});
     res.json(data);
 });
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Hace que Express sirva los archivos de la app compilada por Vite
+app.use(express.static(path.join(__dirname, './dist')));
+
+// Cualquier ruta que no sea de la API, cargará el frontend de React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './dist/index.html'));
+});
 app.listen(port, () => {
     console.log(`Backend de MARSI corriendo en http://localhost:${port}`);
 });
