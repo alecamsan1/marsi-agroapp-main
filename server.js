9ce17243578abeefac26864708d7213cc030153f
alecamsan1 <alecamsan@alum.us.es>
2026-05-22 15:41:30 +0200

    Fix: adaptada ruta comodín a la nueva versión de Express

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, './dist')));
 
 // Cualquier ruta que no sea de la API, cargará el frontend de React
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 app.listen(port, () => {
